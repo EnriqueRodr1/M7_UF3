3a0c68b28ff98af835eba4dcef81fae2248a558a
Enrique Alejandroo Rodriguez <2024_enrique.rodriguez@iticbcn.cat>
2025-04-11 19:55:59 +0200

    m7 arreglado

--- ACTIVITAT13/centre/urls.py
+++ ACTIVITAT13/centre/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('students/', views.students, name='students'),
+    path('students/<int:id>/', views.students_detalle, name='students_detalle'),
     path('teachers/', views.teachers, name='teachers'),
+    path('teachers/<int:id>/', views.teachers_detalle, name='teachers_detalle'),
 ]
--- ACTIVITAT13/centre/views.py
+++ ACTIVITAT13/centre/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Student, Teacher
 
 def index(request):
@@ -8,6 +8,14 @@ def students(request):
     students = Student.objects.all()
     return render(request, 'centre/students.html', {'students': students})
 
+def students_detalle(request, id):
+    student = get_object_or_404(Student, pk=id)
+    return render(request, 'centre/students_detalle.html', {'student': student})
+
 def teachers(request):
     teachers = Teacher.objects.all()
     return render(request, 'centre/teachers.html', {'teachers': teachers})
+
+def teachers_detalle(request, id):
+    teacher = get_object_or_404(Teacher, pk=id)
+    return render(request, 'centre/teachers_detalle.html', {'teacher': teacher})

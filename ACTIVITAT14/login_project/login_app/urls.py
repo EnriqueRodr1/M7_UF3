efe93f32eae4c51a22b41d734f6e98f59ecde018
EnriqueRodr1 <2024_enrique.rodriguez@iticbcn.cat>
2025-04-30 18:57:34 +0200

    Update urls.py

--- ACTIVITAT14/login_project/login_app/urls.py
+++ ACTIVITAT14/login_project/login_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),  # 👈 esta línea solo funciona si login_view está definida
+    path('', views.login_view, name='login'),
     path('login_session/', views.login_session_view, name='login_session'),
     path('inicio/', views.pagina_inici, name='pagina_inici'),
     path('logout/', views.logout_view, name='logout'),
